d7fe962777e308ccfb212eaeb7eb0dd82b293b2f
Keane <e0425601@u.nus.edu>
2021-10-16 19:05:43 +0800

    Add controller

--- backend/match/src/match/match.controller.ts
+++ backend/match/src/match/match.controller.ts
@@ -1,5 +1,5 @@
 import { ApiTags } from '@nestjs/swagger';
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Post, Query } from '@nestjs/common';
 import { MatchService } from './match.service';
 import { MatchResponse } from './match-response';
 
@@ -8,6 +8,21 @@ import { MatchResponse } from './match-response';
 export class MatchController {
   constructor(private readonly matchService: MatchService) {}
 
+  /*
+Creates a match data for the user.
+*/
+  @Post()
+  createMatch(
+    @Query('id') id: string,
+    @Query('difficulty') difficulty: string,
+    @Query('language') language: string
+  ): string {
+    return this.matchService.createMatch(id, difficulty, language);
+  }
+
+  /*
+  Gets a match for a user if there is a matching user
+   */
   @Get()
   async getMatch(
     @Query('id') id: string,
